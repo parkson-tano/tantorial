1f2785325bc71235b370f3432196816a010a8830
parkson-tano <tanoparksonsilencer@gmail.com>
2022-09-22 09:54:51 +0100

    account setup

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+from django.utils.translation import gettext as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
+class UserAdmin(BaseUserAdmin):
+    # form = UserChangeForm
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                        'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('user_info'), {'fields': ('profile_pic', 'phone_number', 'student_class',
+            'account_type')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'first_name', 'last_name', 'student_class', 
+        'account_type')
+    list_display = ('email', 'first_name', 'last_name', 'is_superuser', 
+        'account_type')
+    ordering = ('email','student_class' )
+admin.site.register(User, UserAdmin)
--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
 # Create your models here.
+
+ACCOUNT_TYPE = (
+		('student', 'student'),
+		('parent', 'parent'),
+		('school', 'school'),
+		('teacher', 'teacher')
+	)
+
+class User(AbstractUser):
+	email = models.EmailField(_('email address'), unique=True)
+	phone_number = models.CharField(max_length = 24)
+	profile_pic = models.ImageField(null=True, blank=True, upload_to = 'profile_img')
+	student_class = models.CharField(max_length=32, null=True, blank=True)
+	account_type = models.CharField(max_length=32, choices = ACCOUNT_TYPE, default='student')
+	date_created = models.DateTimeField(auto_now_add=True)
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['first_name', 'last_name','username']
+
+	def __str__(self):
+		return "{}".format(self.email)
