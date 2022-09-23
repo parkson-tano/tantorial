e079acba69a7a1c2cfa92185264e47417c6d37ff
Odule Ayanfeoluwa <ayanfeoluwakoyinsola@gmail.com>
2022-09-23 01:32:28 +0100

    Added models for organizational(school) structure

---
+++ backend/account/types.py
@@ -0,0 +1,4 @@
+from django.contrib.auth.models import AbstractBaseUser
+from typing import Type
+
+UserType = Type[AbstractBaseUser]
---
+++ backend/account/utils.py
@@ -0,0 +1,18 @@
+"""
+This module defines a set of utility functions the account app
+"""
+import hashlib
+import time
+from .types import UserType
+
+
+def user_profile_path(instance: UserType, filename: str) -> str:
+    """
+    returns the path for user profile image
+
+    should always return a unique path
+    """
+    extension = filename.split(".").pop()
+    directory_name = f"{instance.email}_{instance.pk}"
+    hash = hashlib.md5(str(time.time()).encode()).hexdigest()
+    return f"images/profile/{directory_name}/{hash}.{extension}"
