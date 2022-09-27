309e6d125365d7ab54260335a1414be9b4054634
Odule Ayanfeoluwa <ayanfeoluwakoyinsola@gmail.com>
2022-09-27 02:33:34 +0100

    started working on using djnago auth app instead of all auth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('chaining/', include('smart_selects.urls')),
     path('', include('mainapp.urls')),
     path('accounts/', include('accounts.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('accounts/', include('allauth.urls')),
 ]
 
