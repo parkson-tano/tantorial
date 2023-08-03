a1b41eb746f127df482bab6259a8922a25e9b978
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-08-03 23:13:20 +0100

    added basename

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -3,18 +3,18 @@ from .views import *
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('teacherprofile', TeacherProfileViewSet)
-router.register('studentprofile', StudentProfileViewSet)
-router.register('parentprofile', GuardianProfileViewSet)
-router.register('studentguardian', StudentGuardianViewSet)
-router.register('studentsubject', StudentSubjectViewSet)
-router.register('subjectteacher', SubjectTeacherViewSet)
-router.register('classteacher', ClassTeacherViewSet)
-router.register('schoolprofile', SchoolProfileViewSet)
-router.register('schoolprofileupdate', SchoolProfileUpdateAPIView)
-router.register('teacherprofileupdate', TeacherProfileUpdateAPIView)
-router.register('studentprofileupdate', StudentProfileUpdateAPIView)
-router.register('parentprofileupdate', GuardianProfileUpdateAPIView)
+router.register('teacherprofile', TeacherProfileViewSet, basename='teacherprofile')
+router.register('studentprofile', StudentProfileViewSet, basename='studentprofile' )
+router.register('parentprofile', GuardianProfileViewSet, basename='parentprofile')
+router.register('studentguardian', StudentGuardianViewSet, basename='studentguardian')
+router.register('studentsubject', StudentSubjectViewSet, basename='studentsubject')
+router.register('subjectteacher', SubjectTeacherViewSet, basename='subjectteacher')
+router.register('classteacher', ClassTeacherViewSet, basename='classteacher')
+router.register('schoolprofile', SchoolProfileViewSet, basename='schoolprofile')
+router.register('schoolprofileupdate', SchoolProfileUpdateAPIView, basename='schoolprofileupdate')
+router.register('teacherprofileupdate', TeacherProfileUpdateAPIView, basename='teacherprofileupdate')
+router.register('studentprofileupdate', StudentProfileUpdateAPIView, basename='studentprofileupdate')
+router.register('parentprofileupdate', GuardianProfileUpdateAPIView, basename='parentprofileupdate')
 
 
 urlpatterns = [
